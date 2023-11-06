4108985996db1c61ab1845f8852677c318e37179
David Stobbe <d.stobbe1@share.epsb.ca>
2023-11-06 13:30:23 -0700

    totWords

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 import numpy as np
 import pandas as pd
+import json
 df = pd.read_csv('tmdb_5000_movies.csv')
-df.head()
+
+#Genres, keywords, overview
+
+genreObjects = df['genres'].tolist()
+genres = []
+keywordsObjects = df['keywords'].tolist()
+keywords = []
+
+for o in genreObjects:
+    genres.append([])
+    for genre in json.loads(o):
+        genres[-1].append(genre["name"])
+
+for o in keywordsObjects:
+    keywords.append([])
+    for keyword in json.loads(o):
+        keywords[-1].append(keyword["name"])
+
+print(keywords)
+overview = df['overview'].tolist()
+
+
+titles = df['title'].tolist()
+words = [genres[i] + keywords[i] for i in range(len(titles))]
+# for i in range(len(titles)):
+#     words.append([])
+    
+totWords = []
+for row in words:
+    totWords.extend(row)
+
+
+
 #tfidfvectorizer expects one string per "document"
 # Transform the strings using TF-IDF
 #Assume query is an existing movie in database
